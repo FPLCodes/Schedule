0403dfe8eb391a49f8adda3a240798e36b79e63a
FPLCodes <59481822+FPLCodes@users.noreply.github.com>
2020-01-18 12:02:19 +0800

    Add files via upload
    - Background animations
    - Countdown timer

---
+++ script.js
@@ -0,0 +1,42 @@
+var countDownDate = new Date("Mar 9, 2020").getTime();
+
+var x = setInterval(function () {
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days-anim").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days-anim").innerHTML = "Oops too late!";
+    }
+}, 1000);
+
+var newCountDownDate = new Date("Apr 23, 2020").getTime();
+
+var y = setInterval(function () {
+
+    var now = new Date().getTime();
+    var distance = newCountDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days-anim1").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(y);
+        document.getElementById("days-anim1").innerHTML = "Oops too late!";
+    }
+}, 1000);
+
